00fbb52de8c2ddc494113ce2858cbd958d918dc7
Trecto34 <trecto34@protonmail.com>
2026-06-27 07:09:54 -0300

    Atualização do Main.java/Finalização do Nivel Mestre

--- Main.java
+++ Main.java
@@ -1,20 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        Personagem heroi1 = new Guerreiro("Arthus", "Guerreiro", 1, 100, 10.5);
-        Personagem heroi2 = new Mago("Elenara", "Mago", 5, 80, 12.5);
+	public static void main(String[] args) {
+		Personagem heroi1 = new Guerreiro("Arthus", "Guerreiro", 1, 100, 10.5);
+		Personagem heroi2 = new Mago("Elenara", "Mago", 5, 80, 12.5);
 
-	Personagem[] equipe = new Personagem[2];
+		Grupo grupoA = new Grupo();
+		Grupo grupoB = new Grupo();
 
-	equipe[0] = heroi1;
-	equipe[1] = heroi2;
+		grupoA.adicionarPersonagem(heroi1);
+		grupoB.adicionarPersonagem(heroi2);
 
-	System.out.println("-- Iniciando a Batalha --");
+		System.out.println("=== EQUIPE A ===");
+		grupoA.listarPersonagens();
 
-	for (Personagem p : equipe) {
-		p.exibirStatus();
-		p.usarHabilidadeEspecial();
+		System.out.println("=== EQUIPE B ===");
+		grupoB.listarPersonagens();
+
+		grupoA.batalhar(heroi1, heroi2);
 	}
-
-	System.out.println("--------------------------");
-    }
 }
